d3e02023c662cab30a2f5c41f25896fea675a826
Alaa Osman <alaa.osmana@gmail.com>
2020-12-11 13:05:25 +0200

    setup server and mov products to api

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
 import Product from '../components/Product.js';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
   const productsList = products.map(product => (
     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
       <Product product={product} />
     </Col>
   ));
-  return <Row>{productsList}</Row>;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>{productsList}</Row>
+    </>
+  );
 };
 
 export default HomeScreen;
--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
+import axios from 'axios';
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(p => p._id === match.params.id);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`);
+
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match]);
 
   return (
     <>
@@ -23,7 +32,7 @@ const ProductScreen = ({ match }) => {
             </ListGroup.Item>
             <ListGroup.Item>
               <Rating
-                value={product.rating}
+                value={product.rating || 0}
                 text={`${product.numReviews} reviews`}
               />
             </ListGroup.Item>
