c808ac1879260d72922379c05ef88ce41ac9fa1f
Alaa Osman <alaa.osmana@gmail.com>
2020-12-11 11:06:48 +0200

    add router and product screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './screens/HomeScreen';
+import Product from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Home />
+          <Route exact path='/' component={Home} />
+          <Route path='/product/:id' component={Product} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
