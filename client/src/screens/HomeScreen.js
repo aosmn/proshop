5e7abfdc2895b67373454b0bf341d1a9b6cc986e
Alaa Osman <alaa.osmana@gmail.com>
2020-12-12 20:12:32 +0200

    added redux to home screen and product screen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product.js';
-import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../redux/actions/productActions';
+
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const { loading, error, products } = productList;
   const productsList = products.map(product => (
     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
       <Product product={product} />
@@ -13,16 +18,18 @@ const HomeScreen = () => {
   ));
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>{productsList}</Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>{productsList}</Row>
+      )}
     </>
   );
 };
