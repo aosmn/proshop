295a9672eae91a609c88d0549d17f0a180e43f61
Alaa Osman <alaa.osmana@gmail.com>
2020-12-14 12:25:53 +0200

    Created edit profile route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 } from '../controllers/user.controller.js';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
@@ -10,6 +11,9 @@ const router = express.Router();
 router.route('/').post(registerUser);
 router.post('/login', authUser);
 
-router.route('/profile').get(protect, getUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
